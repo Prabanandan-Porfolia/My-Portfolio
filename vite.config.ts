686478cf83a106f25f089f7b109a945318e849e3
Prabanandan-Porfolia <prabaripd@gmail.com>
2026-03-17 18:15:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   // Set base to your repo name for GitHub Pages
-  // Example: '/portfolio/'
+  // Example: '/My-Portfolio/'
   base: "/My-Portfolio/",
   server: {
     host: "::",
